5aafe247e77d9a1187b41a650b239f87b0fcd0d9
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-05 12:09:39 +0200

    Create createDB.sql

---
+++ createDB.sql
@@ -0,0 +1,20 @@
+-- Table structure for storing the tweets
+CREATE TABLE twitter.tweets (
+  tweetid int8 NOT NULL,
+  tweetcontent jsonb,
+  PRIMARY KEY (tweetid)
+) WITH (OIDS=FALSE);
+
+
+-- some Index on the jsonb datatype
+DROP INDEX idx_tweets_language;
+CREATE INDEX idx_tweets_language 
+ON twitter.tweets ((tweetcontent->>'lang'));
+
+DROP INDEX idx_tweets_writer;
+CREATE INDEX idx_tweets_writer 
+ON twitter.tweets ((tweetcontent->>'screen_name'));
+
+DROP INDEX idx_tweets_timestamp;
+CREATE INDEX idx_tweets_timestamp 
+ON twitter.tweets ((tweetcontent->>'timestamp_ms'));
